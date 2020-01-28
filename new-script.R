99892bd9df93ef2212e491521e750e2fc5322608
Greg Rousell <gregrousell@50-204-120-225-static.hfc.comcastbusiness.net>
2020-01-27 16:07:26 -0800

    Add R Script, .RPorj file and gitignore

---
+++ new-script.R
@@ -0,0 +1,21 @@
+## how jenny might do this in a first exploration
+## purposely leaving a few things to change later!
+
+#' Which libraries does R search for packages?
+.libPaths()
+
+## let's confirm the second element is, in fact, the default library
+.Library
+identical(.Library, .libPaths()[2])
+
+## Huh? Maybe this is an symbolic link issue?
+library(fs)
+identical(path_real(.Library), path_real(.libPaths()[2]))
+
+#' Installed packages
+library(tidyverse)
+ipt <- installed.packages() %>%
+  as_tibble()
+
+## how many packages?
+nrow(ipt)
